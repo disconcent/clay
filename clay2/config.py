a32d71d8cdea9f95e04450e33c03e1567b1f5c85
disconcent <89609518+disconcent@users.noreply.github.com>
2024-02-20 10:16:47 +0700

    Update config.py

--- clay2/config.py
+++ clay2/config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6542544254:AAHNr4TYouDLkQo5o7mb8LMVKSHTIPvd4sE")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "11381402"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "11381402"))
 API_HASH = os.environ.get("API_HASH", "349d6f6868d82dc82c7a9b356051f035")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001545619646"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "0")
@@ -36,28 +36,28 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://mcyhqjyp:u-vk6yHroCSnpRBXdok7UZMmqhQ-Gciu@jelani.db.elephantsql.com/mcyhqjyp")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002098537869"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002125362260"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 # Pesan Awalan /start
 START_MSG = os.environ.get(
     "START_MESSAGE",
-    "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
+    "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>\n\n<b>🛑SPECIAL PROMO GRUB VVIP🛑\n\n✨VVIP INDO 50K✨\n✨VVIP BARAT 50K✨\n✨VVIP JAV 50K✨\n✨VVIP SPECIAL 50K✨\n✨VVIP COLMEK 50K✨\n✨VVIP ONLYFANS 50K✨\n✨VVIP CAMPURAN 50K✨\n✨VVIP BDSM 50K✨\n✨HIJAB 50K✨\n✨PELAJAR 50K✨\n✨VVIP SULTAN 100K✨\n✨VVIP PREMIUM SULTAN 200K✨\n✨VVIP BOCIL 150K✨\nVVIP SULTAN 20000+MEDIA\nVVIP PREMIUM SULTAN 38.000+MEDIA UPDATE SETIAP SAAT\n\n\n\nPAYMENT:\n🤑DANA\n💷OVO\n🛵 GOPAY\n\nKEUNTUNGAN JOIN VIP ⤵️\n✅ BERGARANSI\n✅ NONTON VIDEO TANPA LINK\n✅ PERMANENT\n✅ UPDATE SETIAP SAAT\n✅ CUKUP SEKALI BAYAR NONTON SEPUASNYA\n\n📞ADMIN: @serly69 📞\nᴊɪᴋᴀ ᴀɴᴅᴀ ʟɪᴍɪᴛ ʙɪꜱᴀ ᴘᴄ ʙᴏᴛ ᴄᴜꜱᴛᴏᴍᴇʀ ꜱᴇʀᴠɪᴄᴇ\nᴏʀᴅᴇʀ ᴠɪᴀ ʙᴏᴛ 💐\nʟᴀɴɢꜱᴜɴɢ ᴘᴄ\n└»  @clayasisten_bot</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "6207221547 1962436343 5521355570 6839082735").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu</b>",
+    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu</b>\n\n<b>🛑SPECIAL PROMO GRUB VVIP🛑\n\n✨VVIP INDO 50K✨\n✨VVIP BARAT 50K✨\n✨VVIP JAV 50K✨\n✨VVIP SPECIAL 50K✨\n✨VVIP COLMEK 50K✨\n✨VVIP ONLYFANS 50K✨\n✨VVIP CAMPURAN 50K✨\n✨VVIP BDSM 50K✨\n✨HIJAB 50K✨\n✨PELAJAR 50K✨\n✨VVIP SULTAN 100K✨\n✨VVIP PREMIUM SULTAN 200K✨\n✨VVIP BOCIL 150K✨\nVVIP SULTAN 20000+MEDIA\nVVIP PREMIUM SULTAN 38.000+MEDIA UPDATE SETIAP SAAT\n\n\n\nPAYMENT:\n🤑DANA\n💷OVO\n🛵 GOPAY\n\nKEUNTUNGAN JOIN VIP ⤵️\n✅ BERGARANSI\n✅ NONTON VIDEO TANPA LINK\n✅ PERMANENT\n✅ UPDATE SETIAP SAAT\n✅ CUKUP SEKALI BAYAR NONTON SEPUASNYA\n\n📞ADMIN: @serly69 📞\nᴊɪᴋᴀ ᴀɴᴅᴀ ʟɪᴍɪᴛ ʙɪꜱᴀ ᴘᴄ ʙᴏᴛ ᴄᴜꜱᴛᴏᴍᴇʀ ꜱᴇʀᴠɪᴄᴇ\nᴏʀᴅᴇʀ ᴠɪᴀ ʙᴏᴛ 💐\nʟᴀɴɢꜱᴜɴɢ ᴘᴄ\n└»  @clayasisten_bot</b>",
 )
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
